f70bee14b4b63820e9abf2e4dd7077f3a7dcc299
Alexandre Stanislawski <ast@zenexity.com>
2011-06-11 20:24:42 +0200

    bootstrap registration

---
+++ register.php
@@ -0,0 +1,42 @@
+<?php
+  // Registration form...
+ //Here we save the data  
+?>
+<!doctype html>
+<html>
+<head>
+<title>Registration OK</title>
+<link href='http://fonts.googleapis.com/css?family=Droid+sans' rel='stylesheet' type='text/css'>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+<div id="container" itemscope itemtype="http://schema.org/Event">
+  <div id="logo"><h1 itemprop="name"><span class="logoFallback">Demo.js Paris</span></h1></div>
+  <div id="content" class="shadow" >
+    <h2>Thank you!</h2>
+    <p>May the ancient gods of the scene be with you</p>
+  </div>
+
+  <div id="footer" class="shadow">
+
+    <div id="sponsors" class="left">
+      Thanks to our wonderful sponsors : 
+      <ul>
+        <li id="mozilla"><a href="http://www.mozilla.org"><img src="img/mozilla_wht-small.png" alt="Mozilla"/></a></li>
+        <li id="isart"><a href="http://www.isartdigital.com/"><img src="img/logoIsart.png" alt="Isart Digital" /></a></li>
+      </ul>
+    </div>
+    <div id="people" class="right" style="text-align :right">  
+      This event is brought to you by : 
+      <ul>
+        <li>The french demo scene</li>
+        <li><a href="http://parisjs.org"><img alt="ParisJS community" src="img/logoParisJS_sd.png"/></a></li>
+      </ul>
+    </div>
+    <hr class="space"/>
+    <p>Logo by Made(<a href="http://m4de.com">m4de.com</a>).</p>
+  </div>
+</div>
+</body>
+</html>
